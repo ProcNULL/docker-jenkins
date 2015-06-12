2ec658f7494d317d5b0cdb62ddb6f5a4942c9fc5
Loetkolben <SirLoetkolben@gmail.com>
2015-06-12 23:44:16 +0200

    Fix scripts/start.sh: jenkins.jar -> jenkins.war

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@
 # Run as 'nobody' by default
 [ -z "$JENKINS_UID" ] && JENKINS_UID=65534
 
-sudo -u "$JENKINS_UID" /usr/bin/java $JENKINS_JAVA_ARGS -jar /opt/jenkins.jar $JENKINS_ARGS
+sudo -u "$JENKINS_UID" /usr/bin/java $JENKINS_JAVA_ARGS -jar /opt/jenkins.war $JENKINS_ARGS
