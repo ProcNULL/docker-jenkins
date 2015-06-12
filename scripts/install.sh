94e50a11c85d18f36792b9d7f087bb22a8357e4e
Loetkolben <SirLoetkolben@gmail.com>
2015-06-12 23:36:36 +0200

    Assume 'yes' on apt-get operations in scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 apt-get update
-apt-get dist-upgrade
-apt-get install openjdk-7-jre curl
-apt-get clean
+apt-get dist-upgrade -y
+apt-get install -y openjdk-7-jre curl
+apt-get clean -y
 
 curl -sL http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war >/opt/jenkins.war
 chmod 644 /opt/jenkins.war
