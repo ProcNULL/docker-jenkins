3ee4f9312dd77cb358c05b6a3338558b6eddd999
Chronos <chronos@procnull.de>
2015-07-09 18:32:53 +0200

    Use UID correctly

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,5 @@
 # Run as 'nobody' by default
 [ -z "$JENKINS_UID" ] && JENKINS_UID=65534
 
-sudo -u "$JENKINS_UID" /usr/bin/java $JENKINS_JAVA_ARGS -jar /opt/jenkins.war $JENKINS_ARGS
+chown -R $JENKINS_UID /var/lib/jenkins/
+sudo -u "#$JENKINS_UID" /usr/bin/java $JENKINS_JAVA_ARGS -jar /opt/jenkins.war $JENKINS_ARGS
