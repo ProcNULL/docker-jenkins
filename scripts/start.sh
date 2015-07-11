3023e39c4ce04be091a26648610c2a745e049451
Chronos <chronos@procnull.de>
2015-07-11 12:44:58 +0200

    Create user "jenkins" in /etc/passwd ... Because git won't work without it

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-chown -R $UID "$HOME"
+[ -z "$(cat /etc/passwd | grep jenkins)" ] && useradd -U -d "$HOME" -p 1234 -s /bin/false jenkins
+chown -R jenkins "$HOME"
 cd "$HOME"
-sudo -u "#$UID" /usr/bin/java $JENKINS_JAVA_ARGS -jar /opt/jenkins.war $JENKINS_ARGS
+sudo -u jenkins /usr/bin/java $JENKINS_JAVA_ARGS -jar /opt/jenkins.war $JENKINS_ARGS
