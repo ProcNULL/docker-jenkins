ce440c5818fb27cdbf931d431b934e851ce92336
Chronos <chronos@procnull.de>
2015-07-11 13:33:49 +0200

    Fix UID

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -z "$(cat /etc/passwd | grep jenkins)" ] && useradd -U -d "$HOME" -p 1234 -s /bin/false jenkins
+[ -z "$(cat /etc/passwd | grep jenkins)" ] && useradd -U -u $UID -d "$HOME" -p 1234 -s /bin/false jenkins
 chown -R jenkins "$HOME"
 cd "$HOME"
 sudo -u jenkins /usr/bin/java $JENKINS_JAVA_ARGS -jar /opt/jenkins.war $JENKINS_ARGS
