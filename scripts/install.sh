47587c967af439664ffc9d36014f317232272f94
Loetkolben <SirLoetkolben@gmail.com>
2015-07-12 13:14:39 +0200

    Make Jenkins version configurable
    in scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+#Jenkins version (use "latest" for latest)
+JENKINS_VER="1.609.1"
+
 apt-get update
-apt-get dist-upgrade -y
-apt-get install -y openjdk-7-jre openjdk-7-jdk curl git sudo
+apt-get install -y openjdk-7-jre curl sudo openjdk-7-jdk git
 apt-get clean -y
 
-curl -sL http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war >/opt/jenkins.war
+curl -sL "http://mirrors.jenkins-ci.org/war-stable/$JENKINS_VER/jenkins.war" >/opt/jenkins.war
 chmod 644 /opt/jenkins.war
 
 mkdir -p /jenkins
